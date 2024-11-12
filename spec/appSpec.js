bcbbdb3c64ae2a3dbbd1a81b79e8914733a4a836
vandenberghen <ethan.vandenberghen@student.vinci.be>
2024-11-12 20:51:37 +0100

    refactor: externalise the  main

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -1,12 +1,11 @@
 import { Main } from "../src/app.js";
 
 describe('Berlin Clock - Single Minutes Row', () => {
-    it('should light up the first lamp for 1 minute', () => {
-        const main = new Main();
+    const main = new Main();
+    it('should light up the first lamp for 0 minute', () => {
       expect(main.getSingleMinutesLine("00:00:00")).toBe("OOOO");
     });
     it('should light up the first lamp for 1 minute', () => {
-        const main = new Main();
         expect(main.getSingleMinutesLine("00:01:00")).toBe("YOOO");
       });
     
