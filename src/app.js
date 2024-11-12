8c5d8d79b8e4bc9de206df7d5f1a27a6ab95f1c3
verhoest-arthur <arthur.verhoest@student.vinci.be>
2024-11-12 21:27:56 +0100

    refactor : getFiveMinutesLine and get SingleMinuteLine , externelisation of time

--- src/app.js
+++ src/app.js
@@ -1,20 +1,20 @@
 
 export class Main{
     getSingleMinutesLine(time){
-        const minutes = parseInt(time.split(':')[1], 10);
+        const minutes = this.getMinutes(time);
         const onLamps = minutes % 5;
         return "Y".repeat(onLamps) + "O".repeat(4 - onLamps);
     }
 
     getFiveMinutesLine(time) {
-        const minutes = parseInt(time.split(':')[1], 10);
+        const minutes = this.getMinutes(time);
         const onLamps = Math.floor(minutes / 5);
         let row = "OOOOOOOOOOO";
         row = "Y".repeat(onLamps) + "O".repeat(11 - onLamps);
         return row;
       }
 
-    getMinute(time) {
+    getMinutes(time) {
         const minutes = parseInt(time.split(':')[1], 10);
         return minutes;
     }
