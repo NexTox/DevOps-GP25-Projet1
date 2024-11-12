dba7b547041acf420607873d0dd05b403dd7e39b
vandenberghen <ethan.vandenberghen@student.vinci.be>
2024-11-12 21:01:53 +0100

    refactor: getSingleMinutesLine without return case by case

--- src/app.js
+++ src/app.js
@@ -2,11 +2,7 @@
 export class Main{
     getSingleMinutesLine(time){
         const minutes = parseInt(time.split(':')[1], 10);
-        let row = "OOOO";
-        if (minutes % 5 >= 1) row = "YOOO";
-        if (minutes % 5 >= 2) row = "YYOO";
-        if (minutes % 5 >= 3) row = "YYYO";
-        if (minutes % 5 >= 4) row = "YYYY";
-        return row;
+        const onLamps = minutes % 5;
+        return "Y".repeat(onLamps) + "O".repeat(4 - onLamps);
     }
 }
