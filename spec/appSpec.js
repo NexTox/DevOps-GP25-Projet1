952e784ac8dc7cbea4e1cead74a3511b688f14ab
verhoest-arthur <arthur.verhoest@student.vinci.be>
2024-11-12 21:14:08 +0100

    refactor : change the describe

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -2,20 +2,23 @@ import { Main } from "../src/app.js";
 const main = new Main();
 
 
-describe('Berlin Clock - Single Minutes Row', () => {
-    it('should light up the first lamp for 0 minute', () => {
-    expect(main.getSingleMinutesLine("00:00:00")).toBe("OOOO");
-    });
-    it('should light up the first lamp for 1 minute', () => {
-        expect(main.getSingleMinutesLine("00:01:00")).toBe("YOOO");
-    });
-    it('should light up the first lamp for 2 minute', () => {
-        expect(main.getSingleMinutesLine("00:02:00")).toBe("YYOO");
-    });
-    it('should light up the first lamp for 3 minute', () => {
-        expect(main.getSingleMinutesLine("00:03:00")).toBe("YYYO");
-    });
-    it('should light up the first lamp for 4 minute', () => {
-        expect(main.getSingleMinutesLine("00:04:00")).toBe("YYYY");
-    });
+describe('Berlin Clock ', () => {
+    describe('Single Minutes Row', () => { 
+        it('should light up the first lamp for 0 minute', () => {
+        expect(main.getSingleMinutesLine("00:00:00")).toBe("OOOO");
+        });
+        it('should light up the first lamp for 1 minute', () => {
+            expect(main.getSingleMinutesLine("00:01:00")).toBe("YOOO");
+        });
+        it('should light up the first lamp for 2 minute', () => {
+            expect(main.getSingleMinutesLine("00:02:00")).toBe("YYOO");
+        });
+        it('should light up the first lamp for 3 minute', () => {
+            expect(main.getSingleMinutesLine("00:03:00")).toBe("YYYO");
+        });
+        it('should light up the first lamp for 4 minute', () => {
+            expect(main.getSingleMinutesLine("00:04:00")).toBe("YYYY");
+        });
+});
+
 });
