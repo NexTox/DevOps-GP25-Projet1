d19e113e49f4b6e059f3a855edc59a12064108cb
vandenberghen <ethan.vandenberghen@student.vinci.be>
2024-11-12 20:50:33 +0100

    add: return 'YOOO' when given 1 minutes

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -3,7 +3,11 @@ import { Main } from "../src/app.js";
 describe('Berlin Clock - Single Minutes Row', () => {
     it('should light up the first lamp for 1 minute', () => {
         const main = new Main();
-      expect(main.getSingleMinutesLine("00:00:00")).toBe("OOO");
+      expect(main.getSingleMinutesLine("00:00:00")).toBe("OOOO");
     });
+    it('should light up the first lamp for 1 minute', () => {
+        const main = new Main();
+        expect(main.getSingleMinutesLine("00:01:00")).toBe("YOOO");
+      });
     
   });
